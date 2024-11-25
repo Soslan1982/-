ae08ba3de4e1e806cc59a79ecf6e1c8f88000ede
Soslan1982 <sos102933@gmail.com>
2024-11-26 01:31:14 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 2, 'calibri'
+print(immutable_var)
+# immutable_var[0] = 5
+# print(immutable_var)
+mutable_list =['apple', 'baunty', 'фрукты', "5", "3"]
+print(mutable_list)
+mutable_list[0] = "5"
+print(mutable_list)
