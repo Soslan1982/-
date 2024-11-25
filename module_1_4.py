6b892c2800cc93e586feff0c9fc7f4c51a9a274d
Soslan1982 <sos102933@gmail.com>
2024-11-25 23:48:32 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input("Ваша страна рождения: ")
+print("Вы родились в " + my_string)
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print("Ваша страна рождения: ".replace(' ',""))
+print (my_string[0])
+print (my_string[-1])
